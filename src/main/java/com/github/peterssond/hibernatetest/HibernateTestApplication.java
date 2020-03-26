8fb2eedb87b0babe1736153685273d8bb266e3fb
Daniel Petersson <daniel.petersson@soprasteria.com>
2020-03-26 10:37:55 +0100

    Initial commit

---
+++ src/main/java/com/github/peterssond/hibernatetest/HibernateTestApplication.java
@@ -0,0 +1,13 @@
+package com.github.peterssond.hibernatetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateTestApplication.class, args);
+    }
+
+}
